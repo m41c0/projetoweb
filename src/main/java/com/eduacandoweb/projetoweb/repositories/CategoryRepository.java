839a77b3ac74e76c70f2a1c7961359204f40f5f9
Maicon Ferreira <m41c0n@outlook.com>
2021-07-15 00:56:55 -0300

    Category

---
+++ src/main/java/com/eduacandoweb/projetoweb/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.eduacandoweb.projetoweb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eduacandoweb.projetoweb.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
