6cb074249020eb817e543c7478e7f8f669701844
Maicon Ferreira <m41c0n@outlook.com>
2021-08-14 02:26:32 -0300

    Exception handling - findById

--- src/main/java/com/eduacandoweb/projetoweb/services/UserService.java
+++ src/main/java/com/eduacandoweb/projetoweb/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.eduacandoweb.projetoweb.entities.User;
 import com.eduacandoweb.projetoweb.repositories.UserRepository;
+import com.eduacandoweb.projetoweb.services.exceptions.ResourceNotFoundException;
 
 @Service
 
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
